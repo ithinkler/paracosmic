ba8425a186ac2db66c6117ee973df5738f81e19e
Valerie Bright <vbright.dev@gmail.com>
2016-03-06 21:17:55 +1100

    Set up a basic project

---
+++ paracosmic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(paracosmic.Startup))]
+namespace paracosmic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
